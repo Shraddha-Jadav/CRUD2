b6e7ebe280eaa553acfa6cbed2c3e2d8cbd13cda
Shraddha Jadav <shraddha.j@shaligraminfotech.com>
2024-05-03 18:42:43 +0530

    modify model add validation attribute

--- CRUD2/Models/User.cs
+++ CRUD2/Models/User.cs
@@ -11,18 +11,28 @@ namespace CRUD2.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class user
     {
         public int userId { get; set; }
+        [Required]
         public string name { get; set; }
+        [Required]
         public Nullable<System.DateTime> dob { get; set; }
+        [Required]
         public Nullable<bool> gender { get; set; }
+        [Required]
         public string email { get; set; }
+        [Required]
         public string city { get; set; }
+        [Required]
         public string address { get; set; }
+        [Required]
         public string hobbies { get; set; }
+        [Required]
         public string profile_photo { get; set; }
+        [Required]
         public string resume { get; set; }
     }
 
